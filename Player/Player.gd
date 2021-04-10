5c13adf99b24a0250d04f3e4391bcbf5e7a8b8e4
Douglas Willetts <douglasa.willetts@googlemail.com>
2021-04-10 12:47:04 +0100

    Add initial project files

---
+++ Player/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+# Creates variable only when AnimationPlayer is ready
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+# Runs every tic
+# Understanding delta https://kidscancode.org/godot_recipes/basics/understanding_delta/
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	# print(input_vector)
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACCELERATION * delta)
+		# print(input_vector)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+		
+	# Automatically times velocity by delta
+	velocity = move_and_slide(velocity)
